1c839b741398e064c3b719326115c033165c633f
Simon Mahler <simon.mahler@gmail.com>
2012-09-23 17:42:10 -0400

    changes in test data file

--- Script.fsx
+++ Script.fsx
@@ -1,4 +1,4 @@
 ﻿#r @".\bin\Debug\TurboCsvFileTypeProvider.dll"
 let csv = new TurboCsvFileTypeProvider.TurboCsv<"test.csv">()
-csv.Data |> Seq.iter (fun x -> printfn "%A %A %A %A" x.``int field`` x.``decimal field`` x.``date field`` x.``string field``)
+csv.Data |> Seq.iter (fun x -> printfn "%s %d %f %s" (x.Date.ToShortDateString()) x.LocationId x.Temperature x.Description)
 
