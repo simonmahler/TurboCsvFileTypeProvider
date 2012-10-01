0df3f27e441ca114b64499834b4c2a7e3af3b883
Simon Mahler <simon.mahler@gmail.com>
2012-10-01 16:42:28 -0400

    delted the test file and script
    testing is now done using a new test project

--- Script.fsx
+++
@@ -1,4 +0,0 @@
-﻿#r @".\bin\Debug\TurboCsvFileTypeProvider.dll"
-let csv = new TurboCsvFileTypeProvider.TurboCsv<"test.csv">()
-csv.Data |> Seq.iter (fun x -> printfn "%s %d %f %s" (x.Date.ToShortDateString()) x.LocationId x.Temperature x.Description)
-
